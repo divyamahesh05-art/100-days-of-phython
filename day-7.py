8bed27f28ee112a8c1e36642d47cbe44a6ff33d3
divyamahesh05-art <divyamahesh05@gmail.com>
2026-08-13 20:44:14 +0400

    Add files via upload

---
+++ day-7.py
@@ -0,0 +1,25 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==================== RESTART: C:/Users/Acer/Desktop/bill.py ====================
+========================================
+     PATIENT BILLING & DISCOUNT REPORT
+========================================
+Patient: Divya | Age: 32
+Original Bill: 500 AED
+Status: No Discount
+Discount Amount: 0.0 AED
+Final Payable Bill: 500.0 AED
+----------------------------------------
+Patient: Mary | Age: 65
+Original Bill: 500 AED
+Status: Senior Citizen Discount Applied (20%)
+Discount Amount: 100.0 AED
+Final Payable Bill: 400.0 AED
+----------------------------------------
+Patient: Ahmed | Age: 70
+Original Bill: 1000 AED
+Status: Senior Citizen Discount Applied (20%)
+Discount Amount: 200.0 AED
+Final Payable Bill: 800.0 AED
+----------------------------------------
